a666716a8c3880dd6daf1adb1532e2e045296609
Xie Yan <xieyan@microsoft.com>
2022-04-12 18:14:22 +0800

    Add project files.

---
+++ ParallelThreadsBenchmark/ParalleTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBenchmarks
+{
+    public class ParalleTest
+    {
+        public async Task MaxParalle(int maxThreadNum)
+        {
+            var outputList = Enumerable.Range(1, 200).Select(s => $"_{maxThreadNum}_{s}.txt").ToList();
+            await Parallel.ForEachAsync(
+                outputList,
+                new ParallelOptions { MaxDegreeOfParallelism = maxThreadNum },
+                async (output, cancellationToken) =>
+                {
+                    try
+                    {
+                        await Task.Delay(1);
+                        using FileStream fs = new FileStream($"c:\\perftest\\{output}", FileMode.Create);
+                        using StreamWriter w = new StreamWriter(fs, Encoding.UTF8);
+                        w.Write("There are also a lot of different statistics which can be considered. It will be really hard to analyse the summary table, if all of the available statistics will be shown. Fortunately, BenchmarkDotNet has some heuristics for statistics columns and shows only important columns. For example, if all of the standard deviations are zero (we run our benchmarks against Dry job), this column will be omitted. The standard error will be shown only for cases when we are failed to achieve required accuracy level.");
+                    }
+                    catch (Exception)
+                    {
+                        // Do nothing.
+                    }
+                });
+        }
+    }
+}
---
+++ ParallelThreadsBenchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace MyBenchmarks
+{
+    [AllStatisticsColumn]
+    public class ParallePerfTest
+    {
+        private readonly ParalleTest parallelTest;
+
+        public ParallePerfTest()
+        {
+            parallelTest = new ParalleTest();
+        }
+
+        [Benchmark]
+        public async Task MaxParalle16() => await parallelTest.MaxParalle(16);
+
+        [Benchmark]
+        public async Task MaxParalle32() => await parallelTest.MaxParalle(32);
+
+        [Benchmark]
+        public async Task MaxParalle64() => await parallelTest.MaxParalle(64);
+
+        [Benchmark]
+        public async Task MaxParalle128() => await parallelTest.MaxParalle(128);
+
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
